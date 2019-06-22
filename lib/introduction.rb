09773463518de8040e812f60e2650cfbe288b5ab
yassir bennani <ybennani@yahoo.com>
2019-06-22 22:27:44 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,10 +1,11 @@
-name="julia"
-def introduction (name)
-puts  "Hi, my name is #{name}!."
+def introduction(name)
+  puts "Hi, my name is #{name}."
 end
-introduction
-name= "jose", language= "Ruby"
-def introduction_with_language(name,language)
-   "Hi, my name is #{name}! and I am learning to program in #{language}!."
- end
- introduction_with_language
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction_with_language_optional(name, language="Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
